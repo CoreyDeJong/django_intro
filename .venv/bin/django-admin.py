722cc9fbf9df1f94a2d9949c56ba6171cbf01df5
CoreyDeJong <dejongcorey@gmail.com>
2020-06-15 13:33:14 -0700

    initial commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/dejon/Documents/Codefellows/401/labs/django_intro/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
